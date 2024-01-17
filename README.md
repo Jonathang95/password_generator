be2d9674cc28c44c7a06c22bcd51c37abe408764
unknown <jongarcia.realty@gmail.com>
2024-01-17 16:08:11 -0700

    added js

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # password_generator
+# [](https://)
+
+[Github Repository](https://github.com/Jonathang95/Challenge_2)
+
+![](./Assets/images/Screenshot%202024-01-08%20172203.png)
+## Description
+Jonathan Garcia is an aspiring developer on this portfollio page you'll find various different information about himself, his experience and his previous work.
+## Installation
+N/A
+
+## Usage 
+N/A
+
+## Credits
+N/A
+
+## license
+
+
+MIT License
+
+Copyright (c) 2023 Jonathang95
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -1,5 +1,64 @@
-// Assignment Code
+// Assignment code here
+
+
+// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// four Arrays for character types
+var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var numbers = "1234567890".split("");
+var specials = "!@#$%^&*-+".split(""); 
+
+// Function to generate password with questions requarding user input
+function generatePassword() {
+  var finalPass = [];
+
+  // Prompt the user for password length
+  var passwordLength = parseInt(prompt("Enter the desired password length (between 8 and 128):"));
+
+  // Validate the password length
+  while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Please enter a valid password length between 8 and 128.");
+    passwordLength = parseInt(prompt("Enter the desired password length (between 8 and 128):"));
+  }
+
+  var isLowercase = confirm("Do you want lowercase letters?");
+  var isUppercase = confirm("Do you want uppercase letters?");
+  var isNumber = confirm("Do you want numbers?");
+  var isSpecials = confirm("Do you want specials?");
+
+  if (!isLowercase && !isUppercase && !isNumber && !isSpecials) {
+    alert("Please select at least one character type.");
+    return "";
+  }
+
+  // Loop to generate the password based on user preferences and length
+  for (var i = 0; i < passwordLength; i++) {
+    if (isLowercase && finalPass.length < passwordLength) {
+      var randomCharacter = Math.floor(Math.random() * 26);
+      finalPass.push(lowercase[randomCharacter]);
+    }
+
+    if (isUppercase && finalPass.length < passwordLength) {
+      var randomCharacter = Math.floor(Math.random() * 26);
+      finalPass.push(uppercase[randomCharacter]);
+    }
+
+    if (isNumber && finalPass.length < passwordLength) {
+      var randomCharacter = Math.floor(Math.random() * 10);
+      finalPass.push(numbers[randomCharacter]);
+    }
+
+    if (isSpecials && finalPass.length < passwordLength) {
+      var randomCharacter = Math.floor(Math.random() * specials.length);
+      finalPass.push(specials[randomCharacter]);
+    }
+  }
+
+  // Join the array into a string and return
+  return finalPass.join("");
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -7,8 +66,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
